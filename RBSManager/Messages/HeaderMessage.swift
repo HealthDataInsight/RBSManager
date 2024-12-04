079dc5239fdf17e3a6e7ec98c22f3d726c0555ef
Nicholas Robinson <5624261+bottlehall@users.noreply.github.com>
2024-12-04 13:37:15 +0000

    ROS2 Header does not have seq

--- RBSManager/Messages/HeaderMessage.swift
+++ RBSManager/Messages/HeaderMessage.swift
@@ -9,7 +9,6 @@ import Foundation
 import ObjectMapper
 
 public class HeaderMessage: RBSMessage {
-    public var seq: UInt32 = 0
     public var stamp: TimeMessage?
     public var frameId: String?
 
@@ -23,7 +22,6 @@ public class HeaderMessage: RBSMessage {
     }
 
     override public func mapping(map: Map) {
-        seq <- map["seq"]
         stamp <- map["stamp"]
         frameId <- map["frame_id"]
     }
